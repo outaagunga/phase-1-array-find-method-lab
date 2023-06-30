eb9e7efd04162cd26c9d9173144e5b0746117048
outaagunga <jeconiaagunga@gmail.com>
2023-06-30 17:34:35 +0300

    Completed the assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+const record = [
+  { year: "2015", result: "W"},
+  { year: "2014", result: "N/A"},
+  { year: "2013", result: "L"},
+  //...
+]
+function superbowlWin(record) {
+    const winningRecord = record.find(entry => entry.result === "W");
+    return winningRecord ? winningRecord.year : undefined;
+  }
+  
